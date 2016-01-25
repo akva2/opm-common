b8639ba0d3366c996da2dd0ed2b506be3ad90b4c
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2016-01-25 14:58:42 +0100

    stupid

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -5,8 +5,7 @@
 # $2: Path to install into
 # It puts the build directory in CWD.
 
-cmake $1 -DCMAKE_BUILD_TYPE=Release 
-         -DCMAKE_PREFIX_PATH=$2 -DCMAKE_INSTALL_PREFIX=$2
+cmake $1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$2 -DCMAKE_INSTALL_PREFIX=$2
 
 make -j 4
 make install
