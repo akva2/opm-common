86ffcc181e44e9df0b44e2c7c8e7c4c55e999fb9
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2020-02-12 09:12:29 +0100

    changed: use std::filesystem instead of boost::filesystem
    since we still support g++-7, where filesystem is marked experimental,
    we introduce a wrapper header and expose the namespace to use
    as Opm::filesystem.
    
    for gcc we unconditionally link with libstdc++fs in the python bindings.
    the setup.py stuff links as c code, not c++ code, so it is not
    automatically added on any gcc version. this might prove unportable
    later.

---
+++ src/opm/common/utility/FileSystem.cpp
@@ -0,0 +1,49 @@
+/*
+  Copyright 2019 Equinor ASA
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <opm/common/utility/FileSystem.hpp>
+
+#include <random>
+
+namespace Opm
+{
+
+std::string unique_path(const std::string& input)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    auto randchar = [&gen]()
+    {
+        const std::string set = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        std::uniform_int_distribution<> select(0, set.size()-1);
+        return set[select(gen)];
+    };
+
+    std::string ret;
+    for (const char ch : input) {
+        if (ch  == '%')
+            ret += randchar();
+        else
+            ret += ch;
+    }
+
+    return ret;
+}
+
+}
