c0adbcb13113ecd4e54e623cdc310737bf4b6bd7
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2020-02-12 22:18:18 +0100

    s

--- opm/parser/eclipse/Utility/String.hpp
+++ opm/parser/eclipse/Utility/String.hpp
@@ -3,7 +3,9 @@
 
 #include <algorithm>
 #include <cctype>
+#include <sstream>
 #include <string>
+#include <vector>
 
 namespace Opm {
 
@@ -33,5 +35,38 @@ std::string trim_copy(const T& s) {
     return ret.substr(0, end + 1);
 }
 }
+
+inline std::vector<std::string> split_string(const std::string& input,
+                                             char delimiter)
+{
+    std::vector<std::string> result;
+    std::string token;
+    std::istringstream tokenStream(input);
+    while (std::getline(tokenStream, token, delimiter))
+        result.push_back(token);
+
+    return result;
+}
+
+inline std::vector<std::string> split_string(const std::string& input,
+                                             const std::string& delimiters)
+{
+    std::vector<std::string> result;
+    std::string::size_type start = 0;
+    while (start < input.size()) {
+        auto end = input.find_first_of(delimiters, start);
+        if (end == std::string::npos) {
+            result.push_back(input.substr(start));
+            start = input.size();
+        } else {
+          result.push_back(input.substr(start, end-start));
+          start = end + 1;
+        }
+    }
+
+    return result;
+}
+
+
 }
 #endif //OPM_UTILITY_STRING_HPP
